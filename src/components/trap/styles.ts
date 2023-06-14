cea13747a5dc365c3a84b4ab101011894f4f20e4
Luciara-Abreu <luciara.abreu@gmail.com>
2023-06-14 16:49:41 -0300

    add trap

---
+++ src/components/trap/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+const TrapImage = styled.div`
+  @keyframes trap-animation {
+    from {
+      background-position-x: 0;
+    }
+    to {
+      background-position-x: -192px;
+    }
+  }
+
+  position: absolute;
+  bottom: ${21 * 5}px;
+  left: ${42 * 8}px;
+  width: ${48}px;
+  height: 50px;
+  background-image: url(/images/trap.png);
+  background-repeat: no-repeat;
+  //background-position: 0 ${-35}px;
+  animation: trap-animation 1s steps(4) infinite;
+`
+
+export { TrapImage }
