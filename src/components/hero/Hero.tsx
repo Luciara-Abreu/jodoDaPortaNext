b7a356580945c77a10c5b7b686134b9dad9083e3
Luciara-Abreu <luciara.abreu@gmail.com>
2023-06-14 15:31:01 -0300

    updates

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -8,18 +8,4 @@ const Hero = () => {
   )
 }
 export default Hero
-/**
-    <ContainerHero
-      style={{
-        position: 'absolute',
-        bottom: 28 * 1,
-        left: 48 * 1,
-        width: TILE_SIZE,
-        height: 100,
-        backgroundImage: 'url(/images/Hero.png)',
-        backgroundRepeat: 'no-repeat',
-        animation: 'hero-animation 1s steps(4) infinite'
-      }}
-    ></ContainerHero>
 
- */
--- src/settings/constants.ts
+++ src/settings/constants.ts
@@ -1,2 +1,2 @@
 export const TILE_SIZE: any = 48
-export const GAME_SIZE: any = 20 * 48 //960PX
+export const GAME_SIZE: any = 20 * TILE_SIZE //960PX
