55f1e9ea9a8bc22944526d8d1584d68edd967697
Luciara-Abreu <luciara.abreu@gmail.com>
2023-06-14 16:49:49 -0300

    updates

--- src/components/tabuleiro/Tabuleiro.tsx
+++ src/components/tabuleiro/Tabuleiro.tsx
@@ -2,19 +2,23 @@ import { GAME_SIZE } from '@/settings/constants'
 import Hero from '../hero/Hero'
 import { TabuleiroContainer } from './styles'
 import Image from 'next/image'
+import MiniDemon from '../mini.demon/MiniDemon'
+import Demon from '../demon/Demon'
+import Trap from '../trap/trap'
+import Chest from '../chest/chest'
+
 
 
 function Tabuleiro() {
   return (
     <TabuleiroContainer>
       <Hero />
+      <Trap />
+      <Chest />
+      <MiniDemon />
+      <Demon/>
       <Image
-        src="/images/tileset.gif"
-        alt="tabuleiro do jogo"
-        width={GAME_SIZE}
-        height={530}
-        className="tabuleiro"
-      />
+        src="/images/tileset.gif" alt="tabuleiro do jogo" width={GAME_SIZE}  height={530} className="tabuleiro"/>
     </TabuleiroContainer>
   )
 }
