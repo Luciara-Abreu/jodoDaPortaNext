b5471c743a318d0e5fc899153089b85d89d97b0f
Luciara-Abreu <luciara.abreu@gmail.com>
2023-06-15 20:20:51 -0300

    updates button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,38 +1,61 @@
 import { useState } from 'react'
 import { ContainerButton } from "./styles"
-import { HERO_BUTTON_UP, HERO_BUTTON_LEFT } from '@/settings/constants'
+import { BUTTON_UP, BUTTON_LEFT } from '@/settings/constants'
+import Message from '../message/mensage'
+
+export let BUTTON_UP_UPDATE: any = 0
+export let BUTTON_LEFT_UPDATE: any = 0
+
+const ButtonHero: React.FC = () => {
+  const[buttonUp, setButtonUp]= useState(BUTTON_UP)
+  const[buttonLeft, setButtonLeft]= useState(BUTTON_LEFT)
+  let message = "";
 
 
+    const changeUP = () => {
+      setButtonUp( buttonUp + 1 )    
+    }  
 
-const ButtonHero = () => {
-  const[button_position, setButton_position]= useState(HERO_BUTTON_UP)
-  const[left_position, setLeft_position]= useState(HERO_BUTTON_LEFT)
-
-  const changeButtonPostionUp = () => {
-    setButton_position({ x: HERO_BUTTON_UP.x + 1})
+  const changeLeft = () => {
+    setButtonLeft( buttonLeft + 1 )   
   }
 
-  const changeButtonPostionLeft = () => {
-    setLeft_position({ y: HERO_BUTTON_LEFT.y + 1})
+if(buttonUp >=27 && buttonUp <= 43){
+    BUTTON_UP_UPDATE = buttonUp
+  }else{
+    <Message message="Opz! Fim do mapa. Escolha outro caminho." />
   }
 
 
-  const changeButtonPostionDown= () => {
-    setButton_position({ x: HERO_BUTTON_UP.x - 1})
+  if(buttonLeft >=155 && buttonLeft <= 174){
+    BUTTON_LEFT_UPDATE = buttonLeft
+  }else{
+    <Message message="Opz! Fim do mapa. Escolha outro caminho." />
   }
-
-  const changeButtonPostionRight = () => {
-    setLeft_position({ y: HERO_BUTTON_LEFT.y - 1})
-  }
+  
+  console.log('UP => ', BUTTON_UP_UPDATE)
+  console.log('LEFT =>', BUTTON_LEFT_UPDATE)
 
 
   return (
     <ContainerButton>
-    <button className="arrow-button arrow-right" onClick={changeButtonPostionRight}></button>
-    <button className="arrow-button arrow-left" onClick={changeButtonPostionLeft}></button>
-    <button className="arrow-button arrow-up" onClick={changeButtonPostionUp}></button>
-    <button className="arrow-button arrow-down" onClick={changeButtonPostionDown}></button>
+    <button className="arrow-button arrow-up" onClick={changeUP}></button>
+    <button className="arrow-button arrow-left" onClick={changeLeft}></button>
+    <button className="arrow-button arrow-right"></button>
+    <button className="arrow-button arrow-down" ></button>
+
   </ContainerButton>
   )
 }
 export default ButtonHero
+
+/**
+ * 
+  const changeDown= () => {
+    setButtonUp( buttonUp - 1)
+  }
+
+  const changeRight = () => {
+    setButtonLeft( buttonLeft -1)
+  }
+ */
--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -53,6 +53,64 @@ const ContainerButton = styled.div`
   .arrow-button:hover {
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.3);
   }
+
+  //--------------------------------------
+  .scroll-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  transform: translate(0, 0);
+
+  width: 200px;
+  height: 200px;
+}
+
+.scroll {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #f2e6d9;
+  overflow: hidden;
+}
+
+.scroll-inner {
+  position: relative;
+  height: 100%;
+  transform-origin: top center;
+  animation: scroll-open 2s ease-in-out;
+}
+
+.scroll-top,
+.scroll-bottom {
+  width: 100%;
+  height: 50px;
+  background-color: #d9bfae;
+}
+
+.scroll-middle {
+  height: calc(100% - 100px);
+  padding: 20px;
+  background-color: #f2e6d9;
+}
+
+.message {
+  font-size: 16px;
+  color: #514839;
+}
+
+@keyframes scroll-open {
+  0% {
+    transform: scaleY(0);
+  }
+  50% {
+    transform: scaleY(1.2);
+  }
+  100% {
+    transform: scaleY(1);
+  }
+}
 `
 
 export { ContainerButton }
