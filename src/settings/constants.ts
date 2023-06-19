c776c065655e9a0138b862ecc346dbb79a75c818
Luciara-Abreu <luciara.abreu@gmail.com>
2023-06-19 17:32:01 -0300

    update constantes

--- src/settings/constants.ts
+++ src/settings/constants.ts
@@ -4,10 +4,10 @@ export const GAME_SIZE: any = 20 * TILE_SIZE //960PX
 //***********************************************************/
 
 export const POSITION_TOP = 5         // do 1 ao 17  / do 27 ao 43
-export const POSITION_RIGHT =  10   // do 3 ao  21 / do 156 ao 174
-export const POSITION_UP= -10       // do 1 ao 17  / do 27 ao 43
-export const POSITION_LEFT = 5     // do 3 ao  21 / do 156 ao 174
+export const POSITION_RIGHT = 10   // do 3 ao  21 / do 156 ao 174
+export const POSITION_UP= -5       // do 1 ao 17  / do 27 ao 43
+export const POSITION_LEFT = 7     // do 3 ao  21 / do 156 ao 174
 
-export const FIM_DO_MAPA_X = 903
+export const FIM_DO_MAPA_X = 900
 export const FIM_DO_MAPA_Y =  437
 
