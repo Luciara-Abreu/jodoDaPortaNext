ae189d8ae25ee524934effb04b29e168d34eeda4
Luciara-Abreu <luciara.abreu@gmail.com>
2023-07-04 09:14:34 -0300

    refactor code index pges

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,38 +2,19 @@ import Footer from "@/components/footer/Footer";
 import { Body, ContainerApp } from "@/styles/styles";
 import Tabuleiro from "@/components/tabuleiro/Tabuleiro";
 
-import { useState } from "react";
-import {
-  POSITION_LEFT,
-  POSITION_RIGHT,
-  POSITION_TOP,
-  POSITION_UP,
-} from "@/settings/constants";
 import ButtonHeroX from "@/components/button/buttonEixoX/ButtonEixoX";
 import ButtonHeroY from "@/components/button/buttonEixoY/ButtonEixoY";
-import Debugger from "@/Debugger";
+import useButtonPositions from "@/hook/useButtonPosition";
 
 export default function Home() {
-  const [buttonLeft, setButtonLeft] = useState(POSITION_LEFT);
-  const [buttonRight, setButtonRight] = useState(POSITION_RIGHT);
-  const [buttonDown, setButtonDown] = useState(POSITION_TOP);
-  const [buttonUp, setButtonUp] = useState(POSITION_UP);
-
-  const handlePositionUpdateX = (
-    newButtonLeft: number,
-    newButtonRight: number
-  ) => {
-    setButtonLeft(newButtonLeft);
-    setButtonRight(newButtonRight);
-  };
-
-  const handlePositionUpdateY = (
-    newButtonDown: number,
-    newButtonUp: number
-  ) => {
-    setButtonDown(newButtonDown);
-    setButtonUp(newButtonUp);
-  };
+  const {
+    buttonLeft,
+    buttonRight,
+    buttonDown,
+    buttonUp,
+    handlePositionUpdateX,
+    handlePositionUpdateY,
+  } = useButtonPositions();
 
   return (
     <ContainerApp>
