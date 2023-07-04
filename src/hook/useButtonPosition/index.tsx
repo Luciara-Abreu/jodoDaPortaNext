2d9f8e3e6ea326e452bd5a0320cd2898e61dc95f
Luciara-Abreu <luciara.abreu@gmail.com>
2023-07-04 08:48:37 -0300

    refactor code

---
+++ src/hook/useButtonPosition/index.tsx
@@ -0,0 +1,30 @@
+import { POSITION_LEFT, POSITION_RIGHT, POSITION_TOP, POSITION_UP } from '@/settings/constants';
+import { useState } from 'react';
+
+const useButtonPositions = () => {
+  const [buttonLeft, setButtonLeft] = useState(POSITION_LEFT);
+  const [buttonRight, setButtonRight] = useState(POSITION_RIGHT);
+  const [buttonDown, setButtonDown] = useState(POSITION_TOP);
+  const [buttonUp, setButtonUp] = useState(POSITION_UP);
+
+  const handlePositionUpdateX = (newButtonLeft: number, newButtonRight: number) => {
+    setButtonLeft(newButtonLeft);
+    setButtonRight(newButtonRight);
+  };
+
+  const handlePositionUpdateY = (newButtonDown: number, newButtonUp: number) => {
+    setButtonDown(newButtonDown);
+    setButtonUp(newButtonUp);
+  };
+
+  return {
+    buttonLeft,
+    buttonRight,
+    buttonDown,
+    buttonUp,
+    handlePositionUpdateX,
+    handlePositionUpdateY,
+  };
+};
+
+export default useButtonPositions;
