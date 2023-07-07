bc5b04b7bf141f6e4fdf5c957439b8e96484f322
Luciara-Abreu <luciara.abreu@gmail.com>
2023-07-07 19:01:40 -0300

    updates

--- src/Debugger/index.tsx
+++ src/Debugger/index.tsx
@@ -1,31 +1,30 @@
-import { IProps } from "@/interfaces";
-import Tile from "./Tale";
-import { Canvas } from "@/context/canvas/helpers";
+import { IProps } from '@/interfaces'
+import Tile from './Tale'
+import { Canvas } from '@/context/canvas/helpers'
 
 function getCanvasMap() {
-  const tilesArray = [];
+  const tilesArray = []
 
   for (let y = 0; y < Canvas.length; y++) {
-    const canvasY = Canvas[y];
+    const canvasY = Canvas[y]
 
     for (let x = 0; x < canvasY.length; x++) {
-      const canvasYX = canvasY[x];
+      const canvasYX = canvasY[x]
 
-      const position = { x: x, y: y };
-      const text = Canvas[y][x] || canvasYX;
+      const position = { x: x, y: y }
+      const text = Canvas[y][x] || canvasYX
 
-      tilesArray.push(
-        <Tile key="tile" initialPosition={position} text={text} />
-      );
+      const keyTile = `tile_${x}_${y}` // Generate a unique key
+
+      tilesArray.push(<Tile key={keyTile} initialPosition={position} text={text} />)
     }
   }
-  return tilesArray;
+  return tilesArray
 }
 
 function Debugger(props: IProps) {
-  const tiles = getCanvasMap();
-  return <div>{tiles}</div>;
+  const tiles = getCanvasMap()
+  return <div>{tiles}</div>
 }
 
-export default Debugger;
-
+export default Debugger
