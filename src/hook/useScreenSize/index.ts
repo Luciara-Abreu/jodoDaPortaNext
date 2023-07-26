8e2d07610e1dceaad767a3453070fe2292a3a7ce
Luciara Abreu <luciara.abreu@gmail.com>
2023-07-26 17:00:51 -0300

    feat: hero steps limitation on small screen x-axis map using button

--- src/hook/useScreenSize/index.ts
+++ src/hook/useScreenSize/index.ts
@@ -4,6 +4,8 @@ import useWindowDimensions from '../useWindowsDimension';
 const useScreenSize = (breakpoint: number) => {
   const [isSmallScreen, setIsSmallScreen] = useState(false);
   const { width } = useWindowDimensions();
+  // eslint-disable-next-line no-console
+  console.log('useScreenSize', width, isSmallScreen)
 
   useEffect(() => {
     setIsSmallScreen(width <= breakpoint);
