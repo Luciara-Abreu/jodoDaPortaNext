2cadd32e4fb87d4268721180426cede271ad6db0
Luciara Abreu <luciara.abreu@gmail.com>
2023-08-02 19:55:20 -0300

    update hook 2

--- src/hook/useScreenSize/index.ts
+++ src/hook/useScreenSize/index.ts
@@ -5,14 +5,14 @@ const useScreenSize = () => {
   const [isSmallScreen, setIsSmallScreen] = useState(true);
   const { width } = useWindowDimensions();
 
-   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const breakpoints = [1024, 1440, 2560, 1360];
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const breakpoints = [1024, 1440, 2560, 1360, 2040];
 
   useEffect(() => {
     let isSmall = true;
 
     for (let i = 0; i < breakpoints.length; i++) {
-      if (width === breakpoints[i]) {
+      if (width === breakpoints[i] || width > 2040) {
         isSmall = false;
         break;
       }
